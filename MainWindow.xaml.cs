abd510ba60b6c448e5360e452d1a4f88805cddec
R1nful <NaSpame@yandex.ru>
2023-06-28 15:28:47 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Practic_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SplitTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            SplitListBox.ItemsSource = SplitText();
+        }
+        private void ReversTextButton_click(object sender, RoutedEventArgs e)
+        {
+            string[] reverseArr = SplitText().Reverse().ToArray();
+            ReverseListBox.ItemsSource = reverseArr;
+
+            ReverseTextLable.Content = String.Join(" ", reverseArr);
+        }
+
+        private string[] SplitText()
+        {
+            string text = MainText.Text;
+            return text.Split(' ');
+        }
+    }
+}
